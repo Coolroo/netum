17d7fbbd23c09a3c56c585e15333ff294bdcdbd7
Wyatt Stagg <coolrooswag@gmail.com>
2024-02-03 19:09:43 -0400

    add dockerization of api

--- netum_api/src/main.rs
+++ netum_api/src/main.rs
@@ -1,3 +1,47 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use log::info;
+use migration::MigratorTrait;
+use rocket::{ get, launch, routes };
+use sea_orm::{ ConnectOptions, Database, DatabaseConnection, DbErr };
+use serde::{ Deserialize, Serialize };
+
+mod entities;
+
+#[derive(Deserialize, Serialize)]
+struct Error {
+    message: String,
+}
+
+#[get("/")]
+fn index() -> String {
+    format!("Welcome to the API :)")
+}
+
+pub async fn get_db() -> Result<DatabaseConnection, DbErr> {
+    let db = Database::connect(
+        ConnectOptions::new(env::var("DATABASE_URL").expect("Could not load database url"))
+            .max_connections(5)
+            .sqlx_logging(true)
+            .sqlx_logging_level(log::LevelFilter::Trace)
+            .clone()
+    ).await?;
+    info!("Beginning migrations...");
+    migration::Migrator::up(&db, None).await?;
+    Ok(db)
+}
+
+#[launch]
+async fn rocket() -> _ {
+    let db = match get_db().await {
+        Ok(db_connection) => db_connection,
+        Err(e) => panic!("Failed to connect to database: {}", e.to_string()),
+    };
+
+    let can_reach_db = db.ping().await.is_ok();
+    if !can_reach_db {
+        panic!("Failed to connect to database!");
+    }
+
+    rocket::build().manage(db).mount("/", routes![index])
 }
