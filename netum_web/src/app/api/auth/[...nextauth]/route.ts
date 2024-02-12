895b592aae8463f9ed10d32a4be21fc9eb4db6ad
Wyatt Stagg <coolrooswag@gmail.com>
2024-02-12 11:00:10 -0400

    add JWT encryption

--- netum_web/src/app/api/auth/[...nextauth]/route.ts
+++ netum_web/src/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,41 @@
-import NextAuth from "next-auth";
+import NextAuth, { AuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { sign, verify } from "jsonwebtoken";
 
-const authOptions = {
+const JWT_SECRET = process.env.NEXTAUTH_SECRET;
+if (!JWT_SECRET) {
+  throw new Error("Missing env.NEXTAUTH_SECRET");
+}
+
+const authOptions: AuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
     }),
   ],
+  jwt: {
+    encode: async ({ token }) => {
+      if (!token) return "";
+
+      if (token.exp === undefined) {
+        return sign(token, JWT_SECRET, {
+          algorithm: "HS256",
+          expiresIn: "24h",
+        });
+      }
+
+      return sign(token, JWT_SECRET, { algorithm: "HS256" });
+    },
+    // @ts-expect-error
+    decode: async ({ token: tokenStr }) => {
+      if (!tokenStr) return null;
+      return verify(tokenStr, JWT_SECRET);
+    },
+  },
+  session: {
+    strategy: "jwt",
+  },
 };
 
 const handler = NextAuth(authOptions);
