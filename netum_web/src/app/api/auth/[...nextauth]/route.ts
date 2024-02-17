e6e1f672196f08290113d5616dac6652c02f1fbe
Wyatt Stagg <coolrooswag@gmail.com>
2024-02-17 15:20:51 -0400

    update auth

--- netum_web/src/app/api/auth/[...nextauth]/route.ts
+++ netum_web/src/app/api/auth/[...nextauth]/route.ts
@@ -14,36 +14,6 @@ const authOptions: AuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
     }),
   ],
-  jwt: {
-    encode: async ({ token }) => {
-      if (!token) return "";
-
-      if (token.exp === undefined) {
-        return sign(token, JWT_SECRET, {
-          algorithm: "HS256",
-          expiresIn: "24h",
-        });
-      }
-
-      return sign(token, JWT_SECRET, { algorithm: "HS256" });
-    },
-    // @ts-expect-error
-    decode: async ({ token: tokenStr }) => {
-      if (!tokenStr) return null;
-      return verify(tokenStr, JWT_SECRET);
-    },
-  },
-  callbacks: {
-    session: async ({ session, token, user }) => {
-      if (!session?.user || !token?.account) {
-        return session;
-      }
-      session.user.name = token.name;
-      session.user.email = token.email;
-      session.user.image = token.picture;
-      return session;
-    },
-  },
 };
 
 const handler = NextAuth(authOptions);
