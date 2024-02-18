0630ab0f1ea0901829e4a115f503429fa1bd1783
Wyatt Stagg <coolrooswag@gmail.com>
2024-02-18 14:54:13 -0400

    update session

--- netum_web/src/app/api/auth/[...nextauth]/route.ts
+++ netum_web/src/app/api/auth/[...nextauth]/route.ts
@@ -13,6 +13,36 @@ const authOptions: AuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
     }),
   ],
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    jwt: async ({ token, user, account, profile }) => {
+      console.log("here");
+      console.log(token);
+      console.log(user);
+      const isSignIn = user ? true : false;
+      // Add auth_time to token on signin in
+      if (isSignIn) {
+        token.auth_time = Math.floor(Date.now() / 1000);
+      }
+      return Promise.resolve(token);
+    },
+    session: async ({ session, token, user }) => {
+      console.log("here2");
+      console.log(token);
+      console.log(session);
+      if (!session?.user || !token?.account) {
+        return session;
+      }
+
+      session.user.id = token.sub;
+      session.user.name = token.name;
+      session.user.image = token.picture;
+      session.user.email = token.email;
+      return session;
+    },
+  },
 };
 
 const handler = NextAuth(authOptions);
